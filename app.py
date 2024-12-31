b595accd0035280681edada9d5e701ba26382067
kponna <ponnakrishnaveni76@gmail.com>
2024-12-31 07:57:53 +0000

    Continuous deployment

--- app.py
+++ app.py
@@ -81,4 +81,7 @@ async def predict_route(request: Request, file: UploadFile = File(...)):
         raise EnergyGenerationException(e,sys)
      
 if __name__ == "__main__":
-    app_run(app,host="localhost",port=8000)
+    
+    app_run(app,host="0.0.0.0",port=8000) #while runnig aws ec2
+
+    # app_run(app,host="localhost",port=8000) # while running fastapi
